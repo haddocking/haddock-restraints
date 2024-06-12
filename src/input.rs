828f33282da0a01f64e2a3556514fafed03adbba
Rodrigo V Honorato <r.vargashonorato@uu.nl>
2024-06-12 13:54:56 +0200

    allow for relative paths in `read_json_file`

--- src/input.rs
+++ src/input.rs
@@ -1,11 +1,36 @@
 use crate::Interactor;
 use std::fs::File;
 use std::io::BufReader;
+use std::process;
 
 pub fn read_json_file(file_path: &str) -> Result<Vec<Interactor>, Box<dyn std::error::Error>> {
     let file = File::open(file_path)?;
     let reader = BufReader::new(file);
-    let data: Vec<Interactor> = serde_json::from_reader(reader)?;
+    let mut data: Vec<Interactor> = serde_json::from_reader(reader)?;
+
+    // Patch the structure path, they will be relative to the location in which the command is being executed
+    let wd = std::path::Path::new(file_path).parent().unwrap();
+
+    data.iter_mut().for_each(|interactor| {
+        if !interactor.structure().is_empty() {
+            let pdb_path = wd.join(interactor.structure());
+
+            // Make sure this is a valid path
+            if pdb_path.exists() {
+                interactor.set_structure(pdb_path.to_str().unwrap());
+            } else {
+                eprintln!("\n### ERROR LOADING STRUCTURE ###");
+                eprintln!("# The file `{}` does not exist", interactor.structure());
+                eprintln!(
+                    "# If you are using relative paths, they should be relative to `{}`\n",
+                    file_path
+                );
+                process::exit(1);
+            }
+
+            interactor.set_structure(pdb_path.to_str().unwrap());
+        }
+    });
 
     Ok(data)
 }
